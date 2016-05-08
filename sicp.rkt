c91013605d1bd8c1a1aa418f1d44c2c2205ba542
Takahiro Kobayashi <koba925@gmail.com>
2016-05-08 20:27:37 +0900

    1.1.4 Compound Procedures

--- sicp.rkt
+++ sicp.rkt
@@ -2,4 +2,10 @@
 
 (require rackunit)
 
+; 1.1.4 Compound Procedures
 
+(define (square x) (* x x))
+(define (sum-of-squares x y) (+ (square x) (square y)))
+(define (f a) (sum-of-squares (+ a 1) (* a 2)))
+
+(check-eq? (f 5) 136)
