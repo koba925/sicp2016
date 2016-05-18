0471accad8df6d51b75fcf9f76318ba2350e9870
Takahiro Kobayashi <koba925@gmail.com>
2016-05-18 19:53:25 +0900

    Exercise 1.10.

--- sicp.rkt
+++ sicp.rkt
@@ -6,9 +6,9 @@
 
 (define (square x) (* x x))
 (define (sum-of-squares x y) (+ (square x) (square y)))
-(define (f a) (sum-of-squares (+ a 1) (* a 2)))
+;(define (f a) (sum-of-squares (+ a 1) (* a 2)))
 
-(check-eq? (f 5) 136)
+;(check-eq? (f 5) 136)
 
 ; 1.1.6 Conditional Expressions and Predicates
 
@@ -194,3 +194,35 @@
       (+ (dec a) (inc b))))
 
 (check-eq? (o+ 4 5) 9)
+
+; Exercise 1.10.
+
+(define (A x y)
+  (cond ((= y 0) 0)
+        ((= x 0) (* 2 y))
+        ((= y 1) 2)
+        (else (A (- x 1)
+                 (A x (- y 1))))))
+
+(check-eq? (A 1 10) 1024)
+(check-eq? (A 2 4) 65536)
+(check-eq? (A 3 3) 65536)
+
+(define (f n) (A 0 n))
+(define (g n) (A 1 n))
+(define (h n) (A 2 n))
+
+;(f 1)
+;(f 2)
+;(f 3)
+;(f 4)
+;
+;(g 1)
+;(g 2)
+;(g 3)
+;(g 4)
+;
+;(h 1)
+;(h 2)
+;(h 3)
+;(h 4)
