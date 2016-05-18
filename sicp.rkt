86e70f1bc6ab4ce56ccb1ec753445b5469ea6e24
Takahiro Kobayashi <koba925@gmail.com>
2016-05-18 19:09:42 +0900

    1.2.1 Linear Recursion and Iteration

--- sicp.rkt
+++ sicp.rkt
@@ -159,3 +159,18 @@
 
 (check-= (square (sqrt-blockstructure 2)) 2 0.001)
 
+; 1.2.1 Linear Recursion and Iteration
+
+;(define (factorial n)
+;  (if (= n 1)
+;      1
+;      (* n (factorial (- n 1)))))
+
+(define (factorial n)
+  (fact-iter 1 1 n))
+(define (fact-iter product counter max-count)
+  (if (> counter max-count)
+      product
+      (fact-iter (* counter product) (+ counter 1) max-count)))
+(check-eq? (factorial 1) 1)
+(check-eq? (factorial 3) 6)
