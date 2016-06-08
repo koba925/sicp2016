1c6660e9fc2084e0052a2f5e88b028450d4ed723
Takahiro Kobayashi <koba925@gmail.com>
2016-06-08 20:30:12 +0900

    Exercise 1.19.

--- sicp.rkt
+++ sicp.rkt
@@ -414,3 +414,23 @@
   (iter 0 a b))
 
 (check-eq? (fast-iter-* 5 7) 35)
+
+; Exercise 1.19.
+
+(define (fib-ex1-19 n)
+  (define (fib-iter a b p q count)
+    (cond ((= count 0) b)
+          ((even? count)
+           (fib-iter a
+                     b
+                     (+ (* p p) (* q q))
+                     (+ (* 2 p q) (* q q))
+                     (/ count 2)))
+          (else (fib-iter (+ (* b q) (* a q) (* a p))
+                          (+ (* b p) (* a q))
+                          p
+                          q
+                          (- count 1)))))
+  (fib-iter 1 0 0 1 n))
+
+(check-eq? (fib-ex1-19 20) (fib 20))
