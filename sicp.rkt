5e1a57841170c83480548e2a043f42fb0792ea37
Takahiro Kobayashi <koba925@gmail.com>
2016-07-12 20:58:25 +0900

    Exercise 1.35.

--- sicp.rkt
+++ sicp.rkt
@@ -1009,15 +1009,14 @@
   (define (close-enough? v1 v2)
     (< (abs (- v1 v2)) tolerance))
   (define (try guess)
-  (printf "~a~n" guess)
+    ; (printf "~a~n" guess)
     (let ((next (f guess)))
       (if (close-enough? guess next)
           next
           (try next))))
   (try first-guess))
 
-(fixed-point cos 1.0)
-
+; (fixed-point cos 1.0)
 ; (fixed-point (lambda (y) (+ (sin y) (cos y))) 1.0)
 
 (define (sqrt-2 x)
@@ -1029,4 +1028,12 @@
   (fixed-point (lambda (y) (average y (/ x y))) 1.0))
 
 ;(sqrt-3 2)
+
+; Exercise 1.35.
+
+(define φ (fixed-point (lambda (x) (+ 1 (/ 1 x))) 1.0))
+
+φ
+(+ 1 (/ 1 φ))
+
 
