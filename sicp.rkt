f131c46c21ac5f5b19fd8ac807080c79e7b5c033
Takahiro Kobayashi <koba925@gmail.com>
2016-07-17 12:05:37 +0900

    Abstractions and first-class procedures

--- sicp.rkt
+++ sicp.rkt
@@ -1158,4 +1158,22 @@
 
 ; (cube-root-n 3)
 
+; Abstractions and first-class procedures
+
+(define (fixed-point-of-transform g transform guess)
+  (fixed-point (transform g) guess))
+
+(define (sqrt-6 x)
+  (fixed-point-of-transform (lambda (y) (/ x y))
+                            average-damp
+                            1.0))
+
+;(sqrt-6 2.0)
+
+(define (sqrt-7 x)
+  (fixed-point-of-transform (lambda (y) (- (square y) x))
+                            newton-transform
+                            1.0))
+
+;(sqrt-7 2.0)
 
