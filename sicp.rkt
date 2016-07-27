c1418dd3b1542ba98f0354e20605b9650e38a0d6
Takahiro Kobayashi <koba925@gmail.com>
2016-07-27 19:51:39 +0900

    2.1.1 Example: Arithmetic Operations for Rational Numbers

--- sicp.rkt
+++ sicp.rkt
@@ -1323,7 +1323,7 @@
     (lambda (guess) (/ (+ guess (/ x guess)) 2)))
    1.0))
 
-(sqrt-9 2.0)
+;(sqrt-9 2.0)
 
 (define (fixed-point-3 f first-guess)
     ((iterative-improve
@@ -1334,4 +1334,45 @@
 (define (sqrt-10 x)
   (fixed-point-3 (average-damp (lambda (y) (/ x y))) 1.0))
 
-(sqrt-10 2.0)
+;(sqrt-10 2.0)
+
+; 2.1.1 Example: Arithmetic Operations for Rational Numbers
+
+(define (add-rat x y)
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+(define (sub-rat x y)
+  (make-rat (- (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+(define (mul-rat x y)
+  (make-rat (* (numer x) (numer y))
+            (* (denom x) (denom y))))
+(define (div-rat x y)
+  (make-rat (* (numer x) (denom y))
+            (* (denom x) (numer y))))
+(define (equal-rat? x y)
+  (= (* (numer x) (denom y))
+     (* (numer y) (denom x))))
+
+;(define (make-rat n d) (cons n d))
+(define (make-rat n d)
+  (let ((g (gcd n d)))
+    (cons (/ n g) (/ d g))))
+(define (numer x) (car x))
+(define (denom x) (cdr x))
+(define (print-rat x)
+  (newline)
+  (display (numer x))
+  (display "/")
+  (display (denom x)))
+
+(define one-half (make-rat 1 2))
+;(print-rat one-half)
+(define one-third (make-rat 1 3))
+;(print-rat (add-rat one-half one-third))
+;(print-rat (mul-rat one-half one-third))
+;(print-rat (add-rat one-third one-third))
+
+
