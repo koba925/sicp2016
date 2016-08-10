2041245c6f786d29c3f50ad1c2b472e60b5479d3
Takahiro Kobayashi <koba925@gmail.com>
2016-08-10 18:49:19 +0900

    List operations

--- sicp.rkt
+++ sicp.rkt
@@ -1522,9 +1522,45 @@
 ; 2.2.1 Representing Sequences
 
 (define one-through-four (list 1 2 3 4))
-one-through-four
-(car one-through-four)
-(cdr one-through-four)
-(car (cdr one-through-four))
-(cons 10 one-through-four)
-(cons 5 one-through-four)
+;one-through-four
+;(car one-through-four)
+;(cdr one-through-four)
+;(car (cdr one-through-four))
+;(cons 10 one-through-four)
+;(cons 5 one-through-four)
+
+; List operations
+
+(define (list-ref items n)
+  (if (= n 0)
+      (car items)
+      (list-ref (cdr items) (- n 1))))
+(define squares (list 1 4 9 16 25))
+
+(list-ref squares 3)
+
+(define (length items)
+  (if (null? items)
+      0
+      (+ 1 (length (cdr items)))))
+(define odds (list 1 3 5 7))
+
+(length odds)
+
+(define (length-i items)
+  (define (length-iter a count)
+    (if (null? a)
+        count
+        (length-iter (cdr a) (+ 1 count))))
+  (length-iter items 0))
+
+(length-i odds)
+
+(define (append list1 list2)
+  (if (null? list1)
+      list2
+      (cons (car list1) (append (cdr list1) list2))))
+
+(append squares odds)
+(append odds squares)
+
