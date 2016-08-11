9f6372af6ff5bc8108a82ef6b1a070a42a8cfa0b
Takahiro Kobayashi <koba925@gmail.com>
2016-08-11 20:12:29 +0900

    Exercise 2.5. Exercise 2.6.

--- sicp.rkt
+++ sicp.rkt
@@ -1550,11 +1550,51 @@
 (define (car4 z) (cr z 2))
 (define (cdr4 z) (cr z 3))
 
-(car4 (car4 (cons4 (cons4 3 4) (cons4 5 6))))
-(cdr4 (car4 (cons4 (cons4 3 4) (cons4 5 6))))
-(car4 (cdr4 (cons4 (cons4 3 4) (cons4 5 6))))
-(cdr4 (cdr4 (cons4 (cons4 3 4) (cons4 5 6))))
+;(car4 (car4 (cons4 (cons4 3 4) (cons4 5 6))))
+;(cdr4 (car4 (cons4 (cons4 3 4) (cons4 5 6))))
+;(car4 (cdr4 (cons4 (cons4 3 4) (cons4 5 6))))
+;(cdr4 (cdr4 (cons4 (cons4 3 4) (cons4 5 6))))
 
+; Exercise 2.6.
+
+(define zero
+  (lambda (f) (lambda (x) x)))
+(define (add-1 n)
+  (lambda (f) (lambda (x) (f ((n f) x)))))
+
+
+(define one (add-1 zero))
+(define two (add-1 one))
+
+((zero add1) 1)
+((one add1) 2)
+((two add1) 3)
+
+;(add-1 zero)
+;((lambda (n) (lambda (f) (lambda (x) (f ((n f) x))))) (lambda (f) (lambda (x) x)))
+;(lambda (f) (lambda (x) (f (((lambda (f) (lambda (x) x)) f) x))))
+;(lambda (f) (lambda (x) (f ((lambda (x) x) x))))
+;(lambda (f) (lambda (x) (f x)))
+;
+;(add-1 (add-1 zero))
+;(add-1 (lambda (f) (lambda (x) (f x))))
+;((lambda (n) (lambda (f) (lambda (x) (f ((n f) x))))) (lambda (f) (lambda (x) (f x))))
+;(lambda (f) (lambda (x) (f (((lambda (f) (lambda (x) (f x))) f) x))))
+;(lambda (f) (lambda (x) (f ((lambda (x) (f x)) x))))
+;(lambda (f) (lambda (x) (f (f x))))
+
+; (define one (lambda (f) (lambda (x) (f x))))
+; (define two (lambda (f) (lambda (x) (f (f x)))))
+
+;(define (+' m n)
+;  (if (=' m zero)
+;      n
+;      (+' (sub-1 m) (add-1 n))))
+
+(define (ch+ n m)
+  ((n add-1) m))
+
+(((ch+ one two) add1) 1)
 
 ;----
 
