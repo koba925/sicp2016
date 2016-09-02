3821302cef02fd42ac9e9279881739dc8588f787
Takahiro Kobayashi <koba925@gmail.com>
2016-09-02 22:50:53 +0900

    Exercise 2.23.

--- sicp.rkt
+++ sicp.rkt
@@ -2010,7 +2010,7 @@
       (cons a (quote ()))
       (cons (car list) (apnd (cdr list) a))))
 
-(apnd '(1 2 3) 4)
+; (apnd '(1 2 3) 4)
 
 (define (square-list-i3 items)
   (define (iter things answer)
@@ -2021,3 +2021,18 @@
 
 ; (square-list-i3 (list 1 2 3 4))
 
+; Exercise 2.23.
+
+(define (for-each proc items)
+  (if (null? items)
+      #t
+      (begin
+        (proc (car items))
+        (for-each proc (cdr items)))))
+
+;(define (for-each proc items)
+;  (cond ((not (null? items))
+;         (proc (car items))
+;         (for-each proc (cdr items)))))
+  
+(for-each (lambda (x) (newline) (display x)) (list 57 321 88))
